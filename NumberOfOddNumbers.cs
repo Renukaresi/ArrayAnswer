8b61ba79dfe0b6fa0b216323edf477d6f5116a13
Renukaresi <renukaresi7@gmail.com>
2019-02-11 13:46:10 +0530

    21st Return the number of OddNumbers

---
+++ NumberOfOddNumbers.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Array_Questions
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+              Console.WriteLine("NumberOfOddNumber: " + NumberOfOddNumbers());
+        }
+         public static int NumberOfOddNumbers()
+        {
+            int[] num = new int[] { 1, 3, 1, 8 };
+            int count = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
